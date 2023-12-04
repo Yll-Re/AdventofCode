f9b96c9db9608a1d23999a779f5404f7f4d78eda
Redi <75811751+Ctrl-Yll@users.noreply.github.com>
2023-12-04 21:17:04 +0100

    Day1 part 1

--- src/day1part1.java
+++ src/day1part1.java
@@ -2,17 +2,58 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class day1part1 {
+
+    public static String repWordNumb(String input){
+        Map<String, String> numberWordMap = new HashMap<>();
+        numberWordMap.put("oneight", "18");
+        numberWordMap.put("twone", "21");
+        numberWordMap.put("eightwo", "82");
+        numberWordMap.put("one", "1");
+        numberWordMap.put("two", "2");
+        numberWordMap.put("three", "3");
+        numberWordMap.put("four", "4");
+        numberWordMap.put("five", "5");
+        numberWordMap.put("six", "6");
+        numberWordMap.put("seven", "7");
+        numberWordMap.put("eight", "8");
+        numberWordMap.put("nine", "9");
+
+        Pattern pattern = Pattern.compile("(oneight|twone|eightwo|one|two|three|four|five|six|seven|eight|nine)");
+
+        Matcher matcher = pattern.matcher(input);
+
+        StringBuilder replacedBuffer = new StringBuilder();
+        while (matcher.find()) {
+            String numberWord = matcher.group();
+            String replacement = numberWordMap.get(numberWord);
+            matcher.appendReplacement(replacedBuffer, ""); // Remove this line
+            if (replacement != null) {
+                replacedBuffer.append(replacement); // Append the replacement directly
+            }
+        }
+        matcher.appendTail(replacedBuffer);
+
+        return replacedBuffer.toString();
+
+    }
+
+
     public static void main(String[] args) {
         int sum = 0;
 
 
-        String filepath = "src/Day1Input";
+        String filepath = "src/Input";
 
         try (BufferedReader br = new BufferedReader (new FileReader(filepath))){
             String line;
             while((line = br.readLine()) != null){
+                line = repWordNumb(line);
                 int leftMost = -1;
                 int rightMost = -1;
                 System.out.println(line);
